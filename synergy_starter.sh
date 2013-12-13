61bc56640220b56ce4fe9d02919cfbc81d81f26d
Christoph Robbert <crobbert@mail.uni-paderborn.de>
2013-12-13 11:01:20 +0100

    Added some different locations like home and university.

--- synergy_starter.sh
+++ synergy_starter.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-
-synergyc --name thinkpad 192.168.1.3
+echo "load de_keybord for synergyc."
+source ~/Skripte/de_keybord_synergy.sh
+if [ $1 =  "zuhause" ]
+then
+  echo "Start synergyc with home configurations."
+  synergyc --name thinkpad 192.168.1.3
+else 
+  echo "Start synergyc with uni configurations."
+  synergyc --name christoph-netbook 131.234.41.2
+fi
